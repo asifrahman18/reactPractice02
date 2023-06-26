7a016378e39f9dd216d00dad764393094ff691f4
Asif Rahman <57532856+asifrahman18@users.noreply.github.com>
2023-06-27 01:30:37 +0600

    8-8

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUsers from "./components/Users/AddUsers";
+import UserList from "./components/UI/UserList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const userListHandler = (uName, uAge) => {
+    setUsersList((prevUserList) => {
+      return [
+        ...prevUserList,
+        { name: uName, age: uAge, id: Math.random.toString() },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUsers />
+      <AddUsers onAddUser={userListHandler} />
+      <UserList users={usersList} />
     </div>
   );
 }
--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,18 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+
 import classes from "./AddUsers.module.css";
 import Card from "../UI/Card/Card";
 import Button from "../UI/Button";
+
 const AddUsers = (props) => {
+  const [enteredUserName, setUserName] = useState("");
+  const [enteredAge, setAge] = useState("");
+
   const addEventHandler = (event) => {
     event.preventDefault();
+
+    if (enteredUserName.trim().length === 0 || enteredAge === 0) {
+      return;
+    }
+
+    if (enteredAge < 0) {
+      return;
+    }
+    props.onAddUser(enteredUserName, enteredAge);
+    setAge("");
+    setUserName("");
   };
+
+  const UserNameHandler = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const AgeHandler = (event) => {
+    setAge(event.target.value);
+  };
+
   return (
     <Card className={classes.input}>
       <form onSubmit={addEventHandler}>
         <label htmlFor="username">User Name</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          value={enteredUserName}
+          onChange={UserNameHandler}
+        />
         <label htmlFor="age">Age</label>
-        <input id="age" type="number" />
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={AgeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
